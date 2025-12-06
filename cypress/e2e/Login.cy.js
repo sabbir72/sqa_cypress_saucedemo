1d20c9c166bf0851571c027a970d14b07faa8270
new <sabbirsce72@gmail.com>
2025-12-06 22:08:36 +0600

    up

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,12 +1,24 @@
-
 describe('Login Test', () => {
+
+  beforeEach(() => {
+    cy.Login();  // every test will be logged in
+  });
+
   it('should login using env variables', () => {
-
-    cy.Login();
     cy.log('Logged in successfully');
-    
-
     cy.url().should('include', '/inventory.html');
     cy.contains('Products').should('be.visible');
   });
+
+  it('Check select product', () => {
+    cy.get('img[data-test="inventory-item-sauce-labs-backpack-img"]').click();
+    // aasert that we are on the product detail page
+    cy.get('div[data-test="inventory-item-name"]').should('have.text', 'Sauce Labs Backpack');
+    // cy.get('div[data-test="inventory-item-price"]').should('have.text', '$29.90');
+
+    cy.get('div[data-test="inventory-item-price"]').then(($price) => {
+      cy.log("Price found: " + $price.text());
+    });
+  });
+
 });
