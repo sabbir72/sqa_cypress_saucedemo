dfc035b82358204a03f6d81a9cc98ca2ded25ea0
new <sabbirsce72@gmail.com>
2025-12-08 23:06:03 +0600

    saucedemo done

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -6,7 +6,6 @@ describe('Login Test', () => {
 
   it('should login using env variables', () => {
     cy.log('Logged in successfully');
-    cy.url().should('include', '/inventory.html');
     cy.contains('Products').should('be.visible');
   });
 
@@ -21,4 +20,32 @@ describe('Login Test', () => {
     });
   });
 
+  it('Add to Cart', () => {
+    cy.get('[data-test="inventory-item-sauce-labs-backpack-img"]').click();
+    cy.get('button[data-test="add-to-cart"]').click();
+    cy.get('a.shopping_cart_link').click();
+    cy.url().should('include', '/cart.html');
+    cy.get('div.inventory_item_name').should('have.text', 'Sauce Labs Backpack');
+    // logout after test
+    cy.get('button#react-burger-menu-btn').click();
+    cy.get('a[data-test="logout-sidebar-link"]').click();
+
+  });
+  
+  it('Cart check', () => {
+    cy.get('[data-test="inventory-item-sauce-labs-backpack-img"]').click();
+    cy.get('button[data-test="add-to-cart"]').click();
+    cy.get('span[data-test="shopping-cart-badge"]').scrollIntoView().should('have.text', '1').click();
+    // assert that we are on the cart page
+    cy.url().should('include', '/cart.html');
+    cy.get('div.inventory_item_name').should('have.text', 'Sauce Labs Backpack');
+
+  });
+
+  it('Logout Test', () => {
+    cy.get('button#react-burger-menu-btn').click();
+    cy.get('a[data-test="logout-sidebar-link"]').click();
+    
+  });
+ 
 });
