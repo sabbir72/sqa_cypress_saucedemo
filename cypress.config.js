27809908ee2c548fedbbb7643b106d0ce96f43ef
new <sabbirsce72@gmail.com>
2025-12-09 22:57:12 +0600

    cypress with allure

--- cypress.config.js
+++ cypress.config.js
@@ -9,16 +9,49 @@
 // });
 
 
+// require('dotenv').config();
+// const { defineConfig } = require('cypress');
+
+// module.exports = defineConfig({
+//   e2e: {
+//     baseUrl: process.env.API_URL,
+//   },
+//   env: {
+//     USER_NAME: process.env.USER_NAME,
+//     PASSWORD: process.env.PASSWORD,
+//   },
+// });
+
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       require('@shelex/cypress-allure-plugin/writer')(on, config);
+//       return config;
+//     },
+//   },
+// });
+
 require('dotenv').config();
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.API_URL,
+
+    setupNodeEvents(on, config) {
+      // Allure Plugin setup
+      require('@shelex/cypress-allure-plugin/writer')(on, config);
+
+      return config;
+    },
   },
+
   env: {
     USER_NAME: process.env.USER_NAME,
     PASSWORD: process.env.PASSWORD,
+    allure: true
   },
 });
 
--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -49,3 +49,95 @@ describe('Login Test', () => {
   });
  
 });
+
+
+// =========================================================================
+// describe('Login Test', () => {
+
+//   beforeEach(() => {
+//     cy.allure().feature('E2E Sauce Demo');
+//     cy.allure().story('User Login');
+//     cy.allure().step('Run custom login command', () => {
+//       cy.Login();
+//     });
+//   });
+
+//   it('should login using env variables', () => {
+//     cy.allure().severity('critical');
+//     cy.allure().step('Verify successful login', () => {
+//       cy.log('Logged in successfully');
+//       cy.contains('Products').should('be.visible');
+//     });
+//   });
+
+//   it('Check select product', () => {
+//     cy.allure().story('Product Details');
+//     cy.allure().step('Click on Backpack image', () => {
+//       cy.get('img[data-test="inventory-item-sauce-labs-backpack-img"]').click();
+//     });
+
+//     cy.allure().step('Verify product detail page', () => {
+//       cy.get('div[data-test="inventory-item-name"]').should('have.text', 'Sauce Labs Backpack');
+//     });
+
+//     cy.allure().step('Read product price dynamically', () => {
+//       cy.get('div[data-test="inventory-item-price"]').then(($price) => {
+//         cy.log("Price found: " + $price.text());
+//       });
+//     });
+//   });
+
+//   it('Add to Cart', () => {
+//     cy.allure().story('Add To Cart');
+    
+//     cy.allure().step('Open product detail page', () => {
+//       cy.get('[data-test="inventory-item-sauce-labs-backpack-img"]').click();
+//     });
+
+//     cy.allure().step('Click Add to Cart', () => {
+//       cy.get('button[data-test="add-to-cart"]').click();
+//     });
+
+//     cy.allure().step('Open Cart Page', () => {
+//       cy.get('a.shopping_cart_link').click();
+//       cy.url().should('include', 'cart.html');
+//       cy.get('div.inventory_item_name').should('have.text', 'Sauce Labs Backpack');
+//     });
+
+//     cy.allure().step('Logout after test', () => {
+//       cy.get('button#react-burger-menu-btn').click();
+//       cy.get('a[data-test="logout-sidebar-link"]').click();
+//     });
+//   });
+
+//   it('Cart check', () => {
+//     cy.allure().story('Cart Validation');
+    
+//     cy.allure().step('Add backpack to cart', () => {
+//       cy.get('[data-test="inventory-item-sauce-labs-backpack-img"]').click();
+//       cy.get('button[data-test="add-to-cart"]').click();
+//     });
+    
+//     cy.allure().step('Verify cart count and open cart', () => {
+//       cy.get('span[data-test="shopping-cart-badge"]').scrollIntoView().should('have.text', '1').click();
+//     });
+    
+//     cy.allure().step('Validate item in cart page', () => {
+//       cy.url().should('include', '/cart.html');
+//       cy.get('div.inventory_item_name').should('have.text', 'Sauce Labs Backpack');
+//     });
+//   });
+
+//   it('Logout Test', () => {
+//     cy.allure().story('Logout');
+    
+//     cy.allure().step('Open side menu', () => {
+//       cy.get('button#react-burger-menu-btn').click();
+//     });
+
+//     cy.allure().step('Click Logout', () => {
+//       cy.get('a[data-test="logout-sidebar-link"]').click();
+//     });
+//   });
+
+// });
